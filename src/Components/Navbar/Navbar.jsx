e50f579503d7af5cd1f4b15b53b2df0c50d7b6e8
mukul <hossenmukul99@gmail.com>
2023-01-16 15:57:53 +0600

    updated home url

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,16 +11,16 @@ const Navbar = () => {
       <nav className='navbar'>
           <div className="container nav">
                <div className="nav-left">
-              <Link to="/react-multipage-website"><img src={Logo} alt="Fit Club" /></Link>
+              <Link to="/"><img src={Logo} alt="Fit Club" /></Link>
           </div>
               <div className="nav-middle">
                   <ul onClick={() => {setMobileMenuToggle(false)}} className={mobileMenuToggle ? 'nav-menu open-menu' : 'nav-menu'}>
                       <li>
-                          <NavLink className='nav-link' to="/react-multipage-website">Home</NavLink>
-                          <NavLink className='nav-link' to="/react-multipage-website/about">About</NavLink>
-                          <NavLink className='nav-link' to="/react-multipage-website/services">Services</NavLink>
-                          <NavLink className='nav-link' to="/react-multipage-website/blog">Blog</NavLink>
-                          <NavLink className='nav-link' to="/react-multipage-website/contact">Contact</NavLink>
+                          <NavLink className='nav-link' to="/">Home</NavLink>
+                          <NavLink className='nav-link' to="/about">About</NavLink>
+                          <NavLink className='nav-link' to="/services">Services</NavLink>
+                          <NavLink className='nav-link' to="/blog">Blog</NavLink>
+                          <NavLink className='nav-link' to="/contact">Contact</NavLink>
                       </li>
                   </ul>
                    <div className="mobile-menu-icons" onClick={() => {setMobileMenuToggle(!mobileMenuToggle)}}>
